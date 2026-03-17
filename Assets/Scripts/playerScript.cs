83030488c596aba16058e682b5bdf7191e1afb70
Nitin Chakradhari <154994765+nkchakradhari780@users.noreply.github.com>
2026-03-17 23:42:42 +0530

    Update playerScript.cs
    Moving towards camera direction

--- Assets/Scripts/playerScript.cs
+++ Assets/Scripts/playerScript.cs
@@ -10,6 +10,9 @@ public class playerScript : MonoBehaviour
     [Header("Player Character controller and Gravity")]
     public CharacterController cController;
 
+    [Header("Player Script Cameras")]
+    public Transform playerCamera;
+
     [Header("Player Jumping and Velocity")]
     public float turnCalmTime = 0.1f;
     float turnCalmVelocity;
@@ -24,13 +27,15 @@ public class playerScript : MonoBehaviour
         float horizontalAxis = Input.GetAxis("Horizontal");
         float verticalAxis = Input.GetAxis("Vertical");
 
-        Vector3 moveDirection = new Vector3(horizontalAxis, 0, verticalAxis).normalized;   
+        Vector3 direction = new Vector3(horizontalAxis, 0, verticalAxis).normalized;   
 
-        if(moveDirection.magnitude >= 0.1f)
+        if(direction.magnitude >= 0.1f)
         {
-            float targetAngle = Mathf.Atan2(moveDirection.x, moveDirection.z) * Mathf.Rad2Deg;
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + playerCamera.eulerAngles.y;
             float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnCalmVelocity, turnCalmTime);
             transform.rotation = Quaternion.Euler(0, angle, 0);
+
+            Vector3 moveDirection = Quaternion.Euler(0, targetAngle, 0) * Vector3.forward;
             cController.Move(moveDirection * playerSpeed * Time.deltaTime);
         }
     }
