d8b4e0f86c105fa5f6769c21fbe7da66fb97c191
Nitin Chakradhari <154994765+nkchakradhari780@users.noreply.github.com>
2026-04-04 19:37:08 +0530

    Implemented Punching Action

--- Assets/Scripts/PickupRifle.cs
+++ Assets/Scripts/PickupRifle.cs
@@ -1,25 +1,84 @@
+using System.Runtime.CompilerServices;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PickupRifle : MonoBehaviour
 {
+    [Header("Player controls")]
+    public InputActionAsset playerControls;
+    InputAction fireAction;
+    InputAction interactAction; 
+
     [Header("Rifle's")]
     public GameObject playerRifle;
     public GameObject pickupRifle;
+    public playerPunch playerPunch;
 
     [Header("Rifle Assign Things")]
     public playerScript player;
     private float radius = 2.5f;
+    private float nextTimeToPunch = 0f;
+    public float punchCharge = 15f; 
+
 
     private void Awake()
     {
         playerRifle.SetActive(false);
     }
 
+    private void OnEnable()
+    {
+        if (playerControls == null)
+        {
+            Debug.LogError("playerControls is NOT assigned!");
+            return;
+        }
+
+        var map = playerControls.FindActionMap("Player");
+
+        if (map == null)
+        {
+            Debug.LogError("Player Action Map NOT found!");
+            return;
+        }
+
+        fireAction = map.FindAction("Attack");
+        interactAction = map.FindAction("Interact");
+
+        if (fireAction == null)
+        {
+            Debug.LogError("Fire action NOT found!");
+            return;
+        }
+
+        if (interactAction == null)
+        {
+            Debug.LogError("Interact action NOT found!");
+            return;
+        }
+
+        fireAction.Enable();
+        interactAction.Enable();
+    }
+
+    private void OnDisable()
+    {
+        fireAction.Disable();
+    }
+
     private void Update()
     {
+        if(fireAction.IsInProgress() && Time.time >= nextTimeToPunch)
+        {
+            nextTimeToPunch = Time.time + 1f / punchCharge;
+
+            playerPunch.Punch();
+
+        }
+
         if (Vector3.Distance(transform.position, player.transform.position) < radius)
         {
-            if (Input.GetKeyDown("f"))
+            if (interactAction.triggered)
             {
                 playerRifle.SetActive(true);
                 pickupRifle.SetActive(false);
--- Assets/Scripts/playerPunch.cs
+++ Assets/Scripts/playerPunch.cs
@@ -2,15 +2,30 @@ using UnityEngine;
 
 public class playerPunch : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    [Header("Player punch var")]
+    public Camera cam;
+    public float damageOf = 10f;
+    public float punchingRange = 5f;
 
-    // Update is called once per frame
-    void Update()
+    [Header("Puching Effects")]
+    public GameObject woodEffect;
+
+    public void Punch()
     {
-        
+        RaycastHit hitInfo;
+
+        if (Physics.Raycast(cam.transform.position, cam.transform.forward, out hitInfo, punchingRange))
+        {
+            Debug.Log(hitInfo.transform.name);
+
+            ObjectToHit hit = hitInfo.transform.GetComponent<ObjectToHit>();
+
+            if (hit != null)
+            {
+                hit.ObjectHitDamage(damageOf);
+                GameObject hitWood = Instantiate(woodEffect, hitInfo.point, Quaternion.LookRotation(hitInfo.normal));
+                Destroy(hitWood, 1f);
+            }
+        }
     }
 }
